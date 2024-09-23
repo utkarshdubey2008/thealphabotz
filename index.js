69622296fc5de0b3646a329f217cfe7c4a45a61d
utkarshdubey2008 <97157730+utkarshdubey2008@users.noreply.github.com>
2024-09-23 14:47:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+const menuIcon = document.querySelector('.menu-icon');
+const navLinks = document.querySelector('.nav-links');
+
+menuIcon.addEventListener('click', () => {
+    menuIcon.classList.toggle('active');
+    navLinks.classList.toggle('active');
+});
+
 // Example JavaScript for interactive elements
 const waifuCards = document.querySelectorAll('.waifu-card');
 
